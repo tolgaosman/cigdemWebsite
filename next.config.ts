28475ac77095d2e2b6408e56d37edc6628b707bc
tolgaosman <tolman2002@gmail.com>
2026-05-19 14:15:29 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/cigdemWebsite',
-  trailingSlash: true,
+  output: 'export', // Projenin statik HTML olarak çıktı vermesini sağlar
+  basePath: '/cigdemWebsite', // GitHub Pages'taki repo adın
   images: {
-    unoptimized: true,
+    unoptimized: true, // Statik exportlarda Next.js Image bileşeninin hata vermesini önler
   },
 };
 
